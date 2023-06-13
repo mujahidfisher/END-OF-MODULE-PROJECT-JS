97993429551268f5fca4c94824a0fa73485ba574
mujahidfisher <fishermujahid@gmail.com>
2023-06-13 10:06:37 +0200

    miner updates

--- script/admin.js
+++ script/admin.js
@@ -1,12 +1,33 @@
 let allItems = []
-let addBtn = document.querySelector("#add-btn")
+let addBtn = document.querySelector("#added")
 let displayItems = document.querySelector("#display-items")
 
+
 addBtn.addEventListener("click", (e) => {
-    e.preventDefault();
+    let newId = document.querySelector("#input-id").value
+    allItems.push(newId)
     
+    let newName = document.querySelector("#input-product").value
+    allItems.push(newName)
+    
+    let newSpecs = document.querySelector("#input-specs").value
+    allItems.push(newSpecs)
+    
+    let newImg = document.querySelector("#url-img")
+    allItems.push(newImg)
+    
+    let newPrice = document.querySelector("#pricing")
+    allItems.push(newPrice)
+    console.log(allItems);
 
-
-
-
+    allItems.forEach(function(e){
+        displayItems.innerHTML = `<tr>
+        <td>${newId}</td>
+        <td>${newName}</td>
+        <td>${newSpecs}</td>
+        <td><img src="${newImg}" alt="product"></td>
+        <td>R${newPrice}</td>
+        <td><button>EDIT</button><button>DELETE</button></td>
+      </tr>`
+    })
 })
