e6a3b017c7d42c86e56c675466d1ace99e795766
mujahidfisher <fishermujahid@gmail.com>
2023-06-13 16:23:52 +0200

    toits

--- script/admin.js
+++ script/admin.js
@@ -1,32 +1,32 @@
 // get empty array.
 function outputData() {
     let key;
-    if(localStorage.getItem("key") == null) {
-        key = [];
-    }else {
-        key = JSON.parse(localStorage.getItem("key"))
-    }
-
-    let displayItems = document.querySelector("#display-items");
-
-    displayItems.innerHTML = "";
-    // fill empty array with the table.
-
-    key.forEach((item) => {
-        displayItems.innerHTML = `
-        <tr>
-        <td>${item.id}</td>
-        <td>${item.product}</td>
-        <td>${item.specs}</td>
-        <td><img src="${item.img}" alt="product" class= "prodpic"></td>
-        <td>R${item.price}</td>
-        <td><button>EDIT</button><button id= "remove-items">DELETE</button></td>
-        </tr>`;
+    if (localStorage.getItem("key")==null) {
+    key = [];
+} else {
+    key = JSON.parse(localStorage.getItem("key"));
+  }
+  
+  let displayItems = document.querySelector("#display-items");
+  
+  displayItems.innerHTML = "";
+  
+  // fill empty array with the table.
+  key.forEach((item) => {
+      displayItems.innerHTML += `
+      <tr>
+      <td>${item.id}</td>
+      <td>${item.product}</td>
+      <td>${item.specs}</td>
+      <td><img src="${item.img}" alt="product" class= "prodpic" loading="lazy"></td>
+      <td>R${item.price}</td>
+      <td><button>EDIT</button><button id= "remove-items">DELETE</button></td>
+      </tr>`;
+    //   console.log(item);
     });
-    
 }
 
-
+document.onload = outputData();
 
 // filling the table with values from the input.
 function addItem() {
@@ -36,31 +36,39 @@ function addItem() {
     let img = document.querySelector("#img").value;
     let price = document.querySelector("#price").value;
     
-    // passing the same array to the localstorage 
-    
     let key;
-    if(localStorage.getItem("key") == null) {
-        key = [];
-    }else {
-        key = JSON.parse(localStorage.getItem("key"))
+  // passing the same array to the localstorage
+  if (localStorage.getItem("key") == null) {
+      key = [];
+    } else {
+        key = JSON.parse(localStorage.getItem("key"));
     }
     
-    // pushing these variables to the array 
+    // pushing these variables to the array
     key.push({
-        id: id,  
+        id: id,
         product: product,
         specs: specs,
         img: img,
-        price: price
-    })
-    localStorage.setItem("key", JSON.stringify(key));
-    outputData();
+        price: price,
+    });
     
-    // clearing the inputs
-    id = document.querySelector("#id").value = "";
-    product = document.querySelector("#product").value = "";
-    specs = document.querySelector("#specs").value = "";
-    img = document.querySelector("#img").value = "";
-    price = document.querySelector("#price").value = "";
+  localStorage.setItem("key", JSON.stringify(key));
+  outputData();
+  
+  // clearing the inputs
+  id = document.querySelector("#id").value = "";
+  product = document.querySelector("#product").value = "";
+  specs = document.querySelector("#specs").value = "";
+  img = document.querySelector("#img").value = "";
+  price = document.querySelector("#price").value = "";
 }
-document.onload = outputData();
+
+// sorting outputs out by first letter of the product name
+let sortItem = document.querySelector("#sort");
+sortItem.addEventListener("click", (e)=> {
+    e.preventDefault()
+    item.sort(function(a, b){return a - b})
+})
+
+
