4cf2f4a077ce531b12258c20e1fbcdef8cf34264
mujahidfisher <fishermujahid@gmail.com>
2023-06-14 09:42:48 +0200

    delete

--- script/admin.js
+++ script/admin.js
@@ -1,3 +1,4 @@
+let key;
 // get empty array.
 function outputData() {
     let key;
@@ -20,7 +21,7 @@ function outputData() {
       <td>${item.specs}</td>
       <td><img src="${item.img}" alt="product" class= "prodpic" loading="lazy"></td>
       <td>R${item.price}</td>
-      <td><button>EDIT</button><button id= "remove-items">DELETE</button></td>
+      <td><button id= "editBtn"onclick='editItem'>EDIT</button><button id= "remove-items" onclick='deleteItem(${JSON.stringify(item)})'>DELETE</button></td>
       </tr>`;
     //   console.log(item);
     });
@@ -35,7 +36,6 @@ function addItem() {
     let specs = document.querySelector("#specs").value;
     let img = document.querySelector("#img").value;
     let price = document.querySelector("#price").value;
-    
     let key;
   // passing the same array to the localstorage
   if (localStorage.getItem("key") == null) {
@@ -62,13 +62,43 @@ function addItem() {
   specs = document.querySelector("#specs").value = "";
   img = document.querySelector("#img").value = "";
   price = document.querySelector("#price").value = "";
+
+  
 }
 
+// delete button getting new array 
+function deleteItem(item){
+    let data = JSON.parse(localStorage.getItem('key'))
+    let x = data.findIndex((y) => {
+        return y.id === item.id
+    })
+    data.splice(x, 1)
+    localStorage.setItem("key", JSON.stringify(data))
+    outputData()
+}
+
+// edit function constructor .this
+class editItem {
+    constructor(item) {
+        this.id = document.querySelector("#id").value;
+        this.product = document.querySelector("#product").value;
+        this.specs = document.querySelector("#specs").value;
+        this.img = document.querySelector("#img").value;
+        this.price = document.querySelector("#price").value;
+    }
+}
+
+
+
+
+
+
+
 // sorting outputs out by first letter of the product name
-let sortItem = document.querySelector("#sort");
-sortItem.addEventListener("click", (e)=> {
-    e.preventDefault()
-    item.sort(function(a, b){return a - b})
-})
+// let sortItem = document.querySelector("#sort");
+// sortItem.addEventListener("click", (e)=> {
+//     e.preventDefault()
+//     item.sort(function(a, b){return a - b})
+// })
 
 
